5c4095ebe36eca9716f44981a6fd84d092ec77f6
andidinata <andi.dinata@outlook.com>
2025-04-23 07:08:19 +0700

    Update home.py

--- home.py
+++ home.py
@@ -2,7 +2,7 @@ import streamlit as st
 # st.set_page_config(layout='wide')
 st.info("Welcome to Colab Coding WebApp, choose the app on the side bar. Hope you find it useful. Uploaded data is not stored in github repository")
 
-pages = {
+# pages = {
     # "Demo" : [
     #     st.Page("./pages/01_Unit Converter.py",title = "Converter",icon="📐"),
     #     st.Page("./pages/02_Online Marketplace.py",title = "Online Store",icon=":material/local_mall:"),
@@ -10,12 +10,12 @@ pages = {
     #     st.Page("./pages/04_Triangle Calculator.py",title = "Triangle",icon=":material/change_history:"),
     #     st.Page("./pages/Camera demo.py",title = "Camera",icon=":material/photo_camera:")
     # ],
-    "Tools" : [
-        st.Page("./pages/05_PDF Rotator.py",title="Rotate PDF",icon=":material/picture_as_pdf:"),
-        st.Page("./pages/06_Inventory Simulation.py",title="Inventory Simulation",icon=":material/inventory:"),
-        st.Page("./pages/06B_Inventory Simulation v2.py",title="Inventory Simulation V2",icon=":material/inventory:"),
-        st.Page("./pages/07_Batch Inventory Simulation.py",title="Batch Inventory Simulation",icon=":material/inventory_2:"),
-        st.Page("./pages/Calculator.py",title="Calculator",icon=":material/apps:")
+    # "Tools" : [
+    #     st.Page("./pages/05_PDF Rotator.py",title="Rotate PDF",icon=":material/picture_as_pdf:"),
+    #     st.Page("./pages/06_Inventory Simulation.py",title="Inventory Simulation",icon=":material/inventory:"),
+    #     st.Page("./pages/06B_Inventory Simulation v2.py",title="Inventory Simulation V2",icon=":material/inventory:"),
+    #     st.Page("./pages/07_Batch Inventory Simulation.py",title="Batch Inventory Simulation",icon=":material/inventory_2:"),
+    #     st.Page("./pages/Calculator.py",title="Calculator",icon=":material/apps:")
     ]
     # "Class Project" : [
     #     st.Page("./pages/Fun Converter.py",title = "Fun Converter",icon=":material/scale:"),
@@ -27,6 +27,6 @@ pages = {
         
     # ],
 
-}
-pg = st.navigation(pages)
-pg.run()
+# }
+# pg = st.navigation(pages)
+# pg.run()
